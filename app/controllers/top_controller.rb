75cba8e688af4a67208bb8422bd9fbc1ada298f5
Ubuntu <ubuntu@ip-172-31-6-129.ec2.internal>
2023-10-19 06:57:24 +0000

    完成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 class TopController < ApplicationController
   def main
     if session[:login_uid] != nil
@@ -8,9 +9,33 @@ class TopController < ApplicationController
   end
 
   def login
-    if params[:uid] == 'kindai' and params[:pass] == 'sanriko'
-      session[:login_uid] = params[:uid]
-      redirect_to top_main_path
+    if User.find_by(uid: params[:uid]) 
+      signup_password = BCrypt::Password.create(params[:pass])
+      if BCrypt::Password.new(signup_password) == params[:pass]
+        session[:login_uid] = params[:uid]
+        redirect_to top_main_path
+      else
+        render "error"
+      end
+    else
+      render "error"
     end
+  end
+  
+  def logout
+    session.delete(:login_uid)
+    redirect_to top_main_path
+  end
+  
+  def new
+    render "signup"
+  end
+  
+  def signup
+    signup_pass = BCrypt::Password.create(params[:pass])
+    user = User.new(uid: params[:uid], pass: signup_pass)
+    user.save
+    session[:login_uid] = params[:uid]
+    redirect_to top_main_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'top/main'
-  get 'top/login'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   get 'top/main'
   post 'top/login'
   root 'top#main'
+  get 'top/logout'
+  get 'top/new'
+  post 'top/signup'
 end
