8ffe020db044c71f2598a286485d0649f3f22dfb
Ubuntu <ubuntu@ip-172-31-6-129.ec2.internal>
2023-10-24 07:45:48 +0000

    a

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,5 @@
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:drop
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
 bundle exec rake db:migrate
